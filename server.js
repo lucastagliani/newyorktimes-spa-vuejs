b6f1fb886baf9807dad724d50dd16869e8ca7706
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-06-28 22:10:09 -0300

    chore: add dist folder for heroku deploy

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get(/.*/, (req, res) => {
+  res.sendfile(`${__dirname}/dist/index.html`);
+});
 
 const port = process.env.PORT || 80;
 app.listen(port);
