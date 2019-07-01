ed4bcbb6c1796de95516b749f3b59dc61bbb172c
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-07-01 01:21:53 -0300

    test: commented structure for services and views tests

---
+++ tests/unit/services/article-service.spec.js
@@ -0,0 +1,9 @@
+// import { expect } from 'chai';
+// import articleService from '@/services/articleService';
+
+// describe('Article service', async () => {
+//   it('should return articles from section "science"', () => {
+//     const articles = articleService.getArticles('science');
+//     expect(articles).to.have.lengthOf.at.least(1);
+//   });
+// });
---
+++ tests/unit/views/home.spec.js
@@ -0,0 +1,20 @@
+// import { expect } from 'chai';
+// import { mount } from '@vue/test-utils';
+// import sinon from 'sinon';
+// import Home from '@/views/Home.vue';
+
+// xdescribe('Home.vue', () => {
+//   it('mount calls method getSections from sectionService', () => {
+//     const spySectionService = sinon.spy();
+//     const spyAnalyticService = sinon.spy();
+//     const wrapper = mount(Home, {
+//       propsData: {
+//         analyticService: spyAnalyticService,
+//         sectionService: spySectionService,
+//       },
+//     });
+//     spySectionService.should.have.been.calledWith('yes');
+//   });
+// });
+
+// TODO: resolve how to test it
